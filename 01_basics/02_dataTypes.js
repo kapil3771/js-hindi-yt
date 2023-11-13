d989996be1189eebaed8ed6eb965570cef0042b3
Kapil Marathe <137314668+kapil3771@users.noreply.github.com>
2023-11-13 09:24:13 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all js code as newer vesion
+
+//alert(3+3) //we are using node js not browser
+
+// console.log(3+
+//     3);  //avoid such practices which destroyes code redability
+
+
+let name = "kapil"
+let age = 18
+let isLoggedIn = true
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
