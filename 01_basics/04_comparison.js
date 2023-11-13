26583a98d316d0f27538c3c240679b3f8a7e7306
Kapil Marathe <137314668+kapil3771@users.noreply.github.com>
2023-11-13 10:11:13 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log(2 > "1");
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null != 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+// strict check (===) also checks datatypes if it is equal or not
+console.log("2" == 2); // true conversion will take place
+console.log("2" === 2); // data types not equal so false
