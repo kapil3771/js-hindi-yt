f6d7831173a2e146f155c1d06eb63baac937fc9d
Kapil Marathe <137314668+kapil3771@users.noreply.github.com>
2023-11-13 09:01:51 +0000

    variables investigation study

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144533
+let accountEmail = "kapil113@google.com"
+var accountPass = "12345"
+Accountcity = "Nandurbar"
+
+// accountId = 69 //not alloweed
+accountEmail = "romanempire123@gmail.com"
+accountPass = "6169"
+Accountcity = "Baramati"
+let Accountstate;
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPass);
+console.log(Accountcity);
+
+console.table([accountId,accountEmail,accountPass,Accountcity,Accountstate]);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
