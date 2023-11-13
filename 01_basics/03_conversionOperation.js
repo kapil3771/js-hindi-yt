a76973bfbf4df2dedc0f1293332ea6ca8d4b9db0
Kapil Marathe <137314668+kapil3771@users.noreply.github.com>
2023-11-13 09:42:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let somenumber = 33
+
+let stringsomenumber = String(somenumber)
+
+console.log(stringsomenumber);
+console.log(typeof(stringsomenumber));
